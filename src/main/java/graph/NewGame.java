8e535877ec2c1b8b011870f2a8cb6fb8748cf87d
Unrike <vladik-sto@ya.ru>
2022-05-30 03:51:31 +0300

    Ready?

--- src/main/java/graph/NewGame.java
+++ src/main/java/graph/NewGame.java
@@ -2,34 +2,40 @@ package graph;
 
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.image.Image;
 import javafx.scene.image.WritableImage;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import logic.Logic;
 
 public class NewGame {
+
+    public static final int STAGE_SIZE = 512;
+
     private Stage stage;
     IdImage[] idImages;
     TileView[] tileViews;
     private Group tileGroup;
+    private final int size;
 
     public NewGame(int size, Image img) {
+        this.size = size;
         stage = new Stage();
         stage.setTitle("Tag");
         stage.getIcons().add(new Image("file:res/icon.png"));
-        stage.setHeight(512);
-        stage.setWidth(512);
+        stage.setMinWidth(STAGE_SIZE);
         stage.setResizable(false);
         idImages = new IdImage[size * size];
         tileViews = new TileView[size * size];
-        int step = 512 / size;
+        int step = STAGE_SIZE / size;
         Logic logic = new Logic(size);
         logic.newGame();
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
                 Image cur = new WritableImage(img.getPixelReader(), j * step, i * step, step, step);
                 if (i * size + j == size * size - 1) {
-                    cur = new Image("file:res/null.jpg");
+                    cur = new Image("file:res/white.jpg");
                 }
                 idImages[i * size + j] = new IdImage(i * size + j, cur);
             }
@@ -39,14 +45,35 @@ public class NewGame {
             for (int j = 0; j < size; j++) {
                 int pos = i * size + j;
                 tileViews[pos] = new TileView(pos, j * step, i * step, step, idImages[logic.getTiles()[pos]]);
+                tileViews[pos].addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
+                    logic.onClick((int) event.getX(), (int) event.getY());
+                    update(logic);
+                    if (logic.isSolved()) {
+                        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                        alert.setTitle("You cool!");
+                        alert.setHeaderText("You won!");
+                        alert.setContentText("You're super-mega-ultra-alfa COOL!");
+                        alert.showAndWait();
+                        stage.close();
+                    }
+                });
                 tileGroup.getChildren().add(tileViews[pos]);
 
             }
         }
         Scene scene = new Scene(tileGroup);
         stage.setScene(scene);
+        stage.setMinHeight(STAGE_SIZE);
         stage.show();
     }
 
+    private void update(Logic logic) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                int pos = i * size + j;
+                tileViews[pos].setIdImage(idImages[logic.getTiles()[pos]]);
 
+            }
+        }
+    }
 }
